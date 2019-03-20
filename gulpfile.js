61df769621a65dcd0c51afadbfa894c4032319c9
Momi-Inu <momiji.inuba@gmail.com>
2019-03-20 00:37:29 -0700

    update refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,13 @@ const { init: sourceInit, write: sourceWrite } = require('gulp-sourcemaps');
 const srcProject = require('gulp-typescript').createProject('tsconfig.json');
 const file = require('gulp-file');
 const clean = require('gulp-clean');
-const gulpSync = require('gulp-sequence');
 
 gulp.task('cleaning', () => {
-    return gulp.src('./build')
+    return gulp.src('./build', { allowEmpty: true })
         .pipe(clean());
 });
 gulp.task('build-ts-develop', () => {
-    return gulp.src('./botsrc/**/*.ts')
+    return gulp.src('./botsrc/**/*.ts', { allowEmpty: true })
         .pipe(sourceInit({ loadMaps: true }))
         .pipe(srcProject()).js
         .pipe(sourceWrite('./'))
@@ -19,34 +18,32 @@ gulp.task('build-ts-develop', () => {
 });
 
 gulp.task('build-ts-production', () => {
-    return gulp.src('./botsrc/**/*.ts')
+    return gulp.src('./botsrc/**/*.ts', { allowEmpty: true })
         .pipe(srcProject()).js
         .pipe(file('type.txt', 'PRODUCTION'))
         .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy-js', () => {
-    return gulp.src('./botsrc/**/*.js')
+    return gulp.src('./botsrc/**/*.js', { allowEmpty: true })
         .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy-static', () => {
-    return gulp.src('./botsrc/**/*.json')
+    return gulp.src('./botsrc/**/*.json', { allowEmpty: true })
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build-develop', gulpSync(
-    'cleaning', [
-        'build-ts-develop',
-        'copy-js',
-        'copy-static'
-    ])
-);
+gulp.task('build-develop', gulp.series(
+    'cleaning',
+    'build-ts-develop',
+    'copy-js',
+    'copy-static'
+));
 
-gulp.task('build-production', gulpSync(
-    'cleaning', [
-        'build-ts-production',
-        'copy-js',
-        'copy-static'
-    ])
-);
+gulp.task('build-production', gulp.series(
+    'cleaning',
+    'build-ts-production',
+    'copy-js',
+    'copy-static'
+))
